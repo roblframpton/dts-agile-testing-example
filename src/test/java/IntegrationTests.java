6e4a6fb4fe11a2c9a90ec9cf0f65e35356095a0d
Blend Dautaj <24854845@stu.mmu.ac.uk>
2025-02-11 12:19:50 +0000

    Wrote unit test #1

--- src/test/java/IntegrationTests.java
+++ src/test/java/IntegrationTests.java
@@ -47,6 +47,20 @@ public class IntegrationTests {
         Assert.assertEquals(0, basket.getItems().size());
     }
 
+    @Test
+    public void WhenRemoveItemThenBasketTotalIsReduced() {
+        Basket basket = new Basket();
+        Menu mainMenu = new MainMenu(basket, new NullTextDisplay());
+        Menu addItemMenu = mainMenu.chooseOption(1);
+        mainMenu = addItemMenu.chooseOption(1);         // Buy Coffee
+        
+        Menu removeItemMenu = mainMenu.chooseOption(2);
+        mainMenu = removeItemMenu.chooseOption(1);      // Remove coffee
+
+        Assert.assertEquals(0, basket.getItemCount());
+        Assert.assertEquals(0, basket.getTotalPrice());
+    }
+
     class NullTextDisplay implements TextDisplay {
         public void displayLine(String line) {}
     }
